a4330273436f5e00195eff93b265514221aea53a
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2023-06-22 11:55:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from '../../libs/three.js-r132/build/three.module.js';
+import * as THREE from 'three.module.js';
 
 document.addEventListener('DOMContentLoaded' , () => {
    const initialize = async() => {
